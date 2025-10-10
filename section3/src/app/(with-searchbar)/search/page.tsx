427bb84d5f2883a1e8a0d5b3f86d83033bd28900
dmweapon <yongsin09140@daum.net>
2025-10-10 13:41:57 +0900

    [replace] src 디렉토리를 샘플코드로 교체

--- section3/src/app/(with-searchbar)/search/page.tsx
+++ section3/src/app/(with-searchbar)/search/page.tsx
@@ -1,10 +1,18 @@
-export default async function page(
-  {
-    searchParams
-  }: {
-    searchParams: Promise<{ q: string }>
-  }) {
-  const { q } = await searchParams
-  console.log(q)
-  return <div>Search 페이지, q = {q}</div>
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const { q } = await searchParams;
+
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
