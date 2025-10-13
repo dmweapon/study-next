6ecfa281626a652cc8fc13194894fae46ac65fd9
dmweapon <yongsin09140@daum.net>
2025-10-13 16:51:50 +0900

    [update] useActionState를 통한 form 태그 통합 관리(중복 요청 방어, 에러처리 등) => Client Component에서 서버 엑션 처리

--- section3/src/actions/create-review.action.ts
+++ section3/src/actions/create-review.action.ts
@@ -1,19 +1,35 @@
 'use server'
-export async function createReviewAction(formData: FormData) {
+import { revalidatePath } from 'next/cache'
+import { delay } from '@/util/delay'
+
+export async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get('bookId')?.toString()
   const content = formData.get('content')?.toString()
   const author = formData.get('author')?.toString()
 
-  if (!content || !author) return
+  if (!bookId || !content || !author) {
+    return {
+      status: false,
+      error: '리뷰 내용과 작성자를 입력해주세요',
+    }
+  }
 
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`, {
+    await delay(2000)
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/1`, {
       method: 'POST',
       body: JSON.stringify({ bookId, content, author }),
     })
-    console.log('[ response.status ] :', response.status)
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+
+    revalidatePath(`/book/${bookId}`)
+    return { status: true, error: '' }
   } catch (e) {
-    console.error(e)
-    return
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${e}`,
+    }
   }
 }
