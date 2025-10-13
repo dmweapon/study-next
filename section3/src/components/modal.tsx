cf9d9fe2478752d48be3aab961160f166af83936
dmweapon <yongsin09140@daum.net>
2025-10-13 23:21:41 +0900

    [update] Intercepting Route + Dialog(Modal) 구현

---
+++ section3/src/components/modal.tsx
@@ -0,0 +1,36 @@
+'use client'
+import style from './modal.module.css'
+
+import { ReactNode, useEffect, useRef } from 'react'
+import { createPortal } from 'react-dom'
+import { useRouter } from 'next/navigation'
+export default function Modal({ children }: { children: ReactNode }) {
+  const dialogRef = useRef<HTMLDialogElement>(null)
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!dialogRef.current?.open) {
+      dialogRef.current?.showModal()
+      dialogRef.current?.scrollTo({
+        top: 0,
+      })
+    }
+  }, [])
+
+  return createPortal(
+    <dialog
+      onClose={() => router.back()}
+      onClick={(e) => {
+        // 모달의 배경을 클릭한 경우 뒤로가기
+        if ((e.target as any).nodeName === 'DIALOG') {
+          router.back()
+        }
+      }}
+      ref={dialogRef}
+      className={style.modal}
+    >
+      {children}
+    </dialog>,
+    document.getElementById('modal-root') as HTMLElement
+  )
+}
